04812d5c7ac4bef88291545ac86be5c9d925a4dc
Daniel <dfespinal7sena@gmail.com>
2024-05-22 14:51:38 -0500

    se implementó bootstrap en las plantillas

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from .models import *
 def listar_fichat(request):
     q=FichaTecnica.objects.all()
     contex={"data":q}
-    return render(request,'app/listar_fichat.html',contex)
+    return render(request,'app/ficha/listar_fichat.html',contex)
 
 def form_fichat(request):
     U=Usuario.objects.all()
@@ -13,5 +13,5 @@ def form_fichat(request):
     M=MaquinaDeCoser.objects.all()
     T=Turno.objects.all()
     S=Suplemento.objects.all()
-    contex={"usuarios":U,"operaciones":O,"maquinas":M,"turno":T,"suplementos":S}
-    return render(request,'app/form_listar.html',contex)
+    contex={"usuarios":U,"operaciones":O,"maquinas":M,"turnos":T,"suplementos":S}
+    return render(request,'app/ficha/form_listar.html',contex)
