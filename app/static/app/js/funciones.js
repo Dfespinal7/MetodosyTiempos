8a252f95e9efc53d7e5f714172f9783a98c1aa1f
Dfespinal7 <dfespinal7@misena.edu.co>
2024-05-23 14:15:05 -0500

    Se crearon funciones para utocompletar formulario

--- app/static/app/js/funciones.js
+++ app/static/app/js/funciones.js
@@ -2,5 +2,14 @@ function actRPM(){
     var tipoMaquina=document.getElementById('tipo_maquina');
     var selectedOption = tipoMaquina.options[tipoMaquina.selectedIndex];
     var rpm = selectedOption.getAttribute('data-rpm');
+    var ppp=selectedOption.getAttribute('data-ppp');
     document.getElementById('rpm').value = rpm;
+    document.getElementById('ppp').value = ppp;
+    
+}
+function actcodOp(){
+    var codigo=document.getElementById("operacion")
+    var seleccion=codigo.options[codigo.selectedIndex]
+    var cod=seleccion.getAttribute('data-cod')
+    codigo_op.value=cod
 }
