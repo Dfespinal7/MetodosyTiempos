8d3160cbea53bea16951b0f97f6dc2676122c1af
Dfespinal7 <dfespinal7@misena.edu.co>
2024-05-27 21:29:20 -0500

    se corrigio apatado de funciones

--- app/static/app/js/funciones.js
+++ app/static/app/js/funciones.js
@@ -5,14 +5,12 @@ function actRPM(){
     var ppp=selectedOption.getAttribute('data-ppp');
     document.getElementById('rpm').value = rpm;
     
-    if (!isNaN(rpm)) { // Verificar si rpm es un número válido
+    
         document.getElementById('rpm').value = rpm;
         console.log(rpm);
         var div = parseFloat(rpm) / 1000;
         document.getElementById("fhsf").value = div; 
-    } else {
-        console.log("El valor de RPM no es un número válido.");
-    }
+    
     
     
 }
@@ -32,14 +30,7 @@ function MTS(){
    var multi=ppp/div
    document.getElementById('resulmt').value = multi.toFixed(2);
 }
-function HSF() {
-    var v1 = parseInt(selectedOption.getAttribute('data-rpm'))
-    var v2 = 1000;
-    var v3 = v1 / v2;
-    document.getElementById('fhsf').value = v3.toFixed(2); // Asegúrate de usar toFixed si quieres redondear el resultado
-    console.log(v1)
-    console.log(v3)
-}
+
 
 // Asegúrate de que esta función esté definida y se llame cuando cambie el tipo de máquina
 document.getElementById('tipo_maquina').addEventListener('change', actRPM);
