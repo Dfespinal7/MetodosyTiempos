a82e2560ca0a0c73fa2794a706325e41acf2c41d
Daniel <dfespinal7sena@gmail.com>
2024-06-04 15:56:55 -0500

    se hicieron modificaciones en el form

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('listar_ficha', listar_fichat,name="listar_ficha"),
     path('form_fichat', form_fichat,name="form_fichat"),
     path('guardar_ficha', guardar_ficha_tecnica,name="guardar_ficha"),
+    path('editar_ficha_form<int:idFicha>', editar_ficha_form,name="editar_ficha_form"),
+
+    
 ]
--- app/views.py
+++ app/views.py
@@ -25,39 +25,58 @@ def form_fichat(request):
 
 def guardar_ficha_tecnica(request):
     if request.method=='POST':
-        
-        usuario=Usuario.objects.get(pk=request.POST.get("usuario"))
-        fecha=request.POST.get("fecha")
-        opera=operacion.objects.get(pk=request.POST.get("operacion"))
-        cod_operacion=request.POST.get("cod_operacion")
-        tipo_maquina=MaquinaDeCoser.objects.get(pk=request.POST.get("tipo_maquina"))
-        ref=request.POST.get("ref")
-        rpm=request.POST.get("rpm")
-        modulo=request.POST.get("modulo")
-        ppp=request.POST.get("ppp")
-        min_turno=Turno.objects.get(pk=request.POST.get("min_turno"))
-        suplementos=Suplemento.objects.get(pk=request.POST.get("suplementos"))
-        tela=request.POST.get("tela")
-        mts=request.POST.get("mts")
-        gtf=Gtf.objects.get(pk=request.POST.get("gtf"))
-        los=request.POST.get("los")
-        nparadas=request.POST.get("nparadas")
-        consta=request.POST.get("consta")
-        hsf=request.POST.get("hsf")
-        ss=request.POST.get("ss")
-        parad=Presicion_de_parada.objects.get(pk=request.POST.get("parad"))
-        tmu=request.POST.get("tmu")
         try:
+            usuario=Usuario.objects.get(pk=request.POST.get("usuario"))
+            fecha=request.POST.get("fecha")
+            opera=operacion.objects.get(pk=request.POST.get("operacion"))
+            cod_operacion=request.POST.get("cod_operacion")
+            tipo_maquina=MaquinaDeCoser.objects.get(pk=request.POST.get("tipo_maquina"))
+            ref=request.POST.get("ref")
+            rpm=request.POST.get("rpm")
+            modulo=request.POST.get("modulo")
+            ppp=request.POST.get("ppp")
+            min_turno=Turno.objects.get(pk=request.POST.get("min_turno"))
+            suplementos=Suplemento.objects.get(pk=request.POST.get("suplementos"))
+            tela=request.POST.get("tela")
+            mts=request.POST.get("mts")
+            gtf=Gtf.objects.get(pk=request.POST.get("gtf"))
+            los=request.POST.get("los")
+            nparadas=request.POST.get("nparadas")
+            consta=request.POST.get("consta")
+            hsf=request.POST.get("hsf")
+            ss=request.POST.get("ss")
+            parad=Presicion_de_parada.objects.get(pk=request.POST.get("parad"))
+            tmu=request.POST.get("tmu")
+        
             ficha=FichaTecnica(idUsuario=usuario,fecha=fecha,operacion=opera,codigo_Operacion=cod_operacion,tipoMaquina=tipo_maquina,ref=ref,rpm=rpm,modulo=modulo,minTurno=min_turno,ppp=ppp,suplementos=suplementos,tipoTela=tela,tiempoMinimoCostura=mts,gtf=gtf,distanciaCostura=los,numeroParadas=nparadas,constante=consta,factorAltaVelocidad=hsf,paradaArranque=ss,presicionParada=parad,tmuCostura=tmu)
             ficha.save()
             messages.success(request,"ficha creada correctamente")
+            
+            
+        
+            num=request.POST.get('numero')
+            cod=request.POST.get('codigo')
+            mov=request.POST.get('movimiento')
+            fre=request.POST.get('frecuencia')
+            dist=request.POST.get('distancia')
+            tmu=request.POST.get('tmu')
+            totalTmu=request.POST.get('total_tmu')
+            tiemposam=request.POST.get('tiempo_sam')
+        
+            fila=FormularioDinamico(fichaTecnica=ficha,Numero=num,codigo=cod,movimiento=mov,frecuencia=fre,distancia=dist,tmu=tmu,totalTmu=totalTmu,tiempoSam=tiemposam)
+            fila.save()
+            messages.success(request, "Ficha y formulario dinámico creados correctamente")
         except Exception as e:
             messages.warning(request, f"error.{e}")
+
+
         return HttpResponseRedirect(reverse("listar_ficha"))
-            
+        
 
 
 
 
+def editar_ficha_form(request,idFicha):
+    pass
 
 
